81622755712bff14c3eb0d3650ab9f0aa2da544c
KhernRhun <Khern.Rhun@gmail.com>
2022-06-04 16:06:42 +0200

    init game commit

--- Exercises_lvl_1.py
+++ Exercises_lvl_1.py
@@ -3,13 +3,16 @@ import random
 number = random.randint(1,51)
 print(f'test: {number}')
 global try_num
-try_num = 1
+try_num = 0
 rerun = 1
 print("Guess the number!")
 print("Type 'stop' to exit")
 while rerun:
-    quess = input("Put the number: ")
-    print(try_num)
+    try:
+        quess = input("Put the number: ")
+    except:
+        print("Blee")
+        continue
     try_num = try_num + 1
     if int(quess) == number:
         break
